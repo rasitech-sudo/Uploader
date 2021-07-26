3e3e9d15d99fceb77965ae661ae991384c61431e
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-07-26 12:34:50 +0700

    Update index.php

--- sc/index.php
+++ sc/index.php
@@ -1,16 +1,3 @@
 <?php
-//header("location:../");
-if ($handle = opendir('.')) {
-
-    while (false !== ($entry = readdir($handle))) {
-
-        if ($entry != "." && $entry != "..") {
-
-            echo "$entry\n";
-        }
-    }
-
-    closedir($handle);
-}
-
+header("location:../");
 ?>
